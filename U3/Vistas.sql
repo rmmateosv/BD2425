97a68a699436f5e0e6e0122cd691fbaa7a152e2f
rmmateosv <rmmateosv@gmail.com>
2024-11-28 13:37:24 +0100

    1128

--- U3/Vistas.sql
+++ U3/Vistas.sql
@@ -3,6 +3,7 @@ use biblioteca;
 
 -- Crear vista que muestre el nombre y telefono de los
 -- usuarios de la biblioteca
+drop view if exists datosus;
 create view datosUS as
 	select nombre, telefono
 		from usuario;
@@ -20,3 +21,38 @@ select * from datosUS
 -- corresponde solamente con un registro de la tabla
 update datosUS set nombre = 'Lucía Sánchez'
 	where nombre = 'Lucía Pérez';
+    
+-- Mostrar los datos del tabla usuario
+select * from usuario;
+-- Vemoos que los datos se han modificado en la tabla
+-- Modifca el nombre de Pedro Pérez por Pedro Ruíz
+-- en la tabla usuario
+update usuario set nombre = 'Pedro Ruíz'
+		where codigo = 1;
+-- Está Pedro Ruíz en la vista?
+select * from datosus;        
+
+-- Crear una nueva tabla en biblioteca
+drop table salas;
+create table salas(
+	id int primary key auto_increment,
+    planta int default 1,
+    sala varchar(10) not null
+)engine innodb;
+insert into salas values
+	(null,1,'Sala 1'),
+    (null,1,'Multimedia'),
+    (null,1,'Wifi'),
+    (null,2,'LibrosM'),
+    (null,2,'LibroA'),
+    (null,2,'Café');
+select * from salas;
+-- Crear una vista con los nombres de salas 
+-- de la planta 1    
+
+-- És actualizable la vista? Por qué?
+
+-- Puedo insertar en la vista salas en la planta 2?
+
+-- Si puedo, cómo harías para que no se pueda?
+-- o si no puedo cómo harías para que se pudiera?
