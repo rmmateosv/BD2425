265db60824627126c8bbb85d4db2e7b376660023
rmmateosv <rmmateosv@gmail.com>
2025-03-17 14:29:40 +0100

    0317

--- U6/01ApuntesRutinas.sql
+++ U6/01ApuntesRutinas.sql
@@ -31,7 +31,80 @@ end//
 
 select holaM2()//
 
--- TIPOS DE PARÁMETROS
+-- TIPOS DE PARÁMETROS: IN/OUT/INOUT
+drop procedure if exists incrementa//
+create procedure incrementa(in v1 integer, out v2 integer,
+							inout v3 integer)
+begin
+	-- Mostrar lo valores inciales de los parámetros
+    select 'Valores iniciales',v1, v2, v3;
+    
+    -- Aumentar en 1 los valores de los parámetros
+    set v1 = v1 + 1;
+    if(v2 is null) then 
+		set v2 = 0;
+	end if;
+    set v2 = v2 + 1;
+    set v3 = v3 + 1;
+    
+    -- Mostrar lo valores modificados
+    select 'Valores modificados',v1, v2, v3;
+end//
+-- Crear 3 variables con valores 100, 200 y 300
+set @uno = 100//
+set @dos = 200//
+set @tres = 300//
+-- Mostrar variables
+select @uno, @dos,  @tres//
+-- Incrementar variables con el procedimiento
+call incrementa(@uno, @dos,  @tres)//
+-- Mostrar variables después de incrementa
+select @uno, @dos,  @tres//
+
+
+-- PROCEDIMIENTO CALCULADORA
+drop procedure if exists calculadora//
+create procedure calculadora(in num1 integer,num2 integer, out resultado varchar(100))
+begin
+	declare suma int;
+    declare resta int;
+    declare mul int;
+    declare divi float;
+    
+    set suma = num1 + num2;
+    set resta = num1 - num2;
+    set mul = num1 * num2;
+    set divi =round(num1 / num2,2);
+    
+    set resultado = concat_ws(' ','Suma',suma,'Resta',resta,
+		'Multiplicación',mul, 'División', divi);
+end//
+
+call calculadora(10,3,@r)//
+select @r//
+
+-- Idem pero con función
+drop function if exists calculadora//
+create function calculadora(num1 integer, num2 integer)
+	returns varchar(100) deterministic
+begin
+	declare suma int;
+    declare resta int;
+    declare mul int;
+    declare divi float;
+    
+    set suma = num1 + num2;
+    set resta = num1 - num2;
+    set mul = num1 * num2;
+    set divi =round(num1 / num2,2);
+    
+    return concat_ws(' ','Suma',suma,'Resta',resta,
+		'Multiplicación',mul, 'División', divi);
+end//    
+select calculadora(6,3)//
+
+-- Realiza una rutina que cree un socio en la bd cooperativa
+-- Realiza una rutina que devuelva el mensaje: El socio  X
 
 
 -- Cambiar de nuevo a ;
