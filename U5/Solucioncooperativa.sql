2f748bd72021bcf5c33b3aa63b44cf5c1f769bbd
rmmateosv <rmmateosv@gmail.com>
2025-03-20 13:36:24 +0100

    0317

--- U5/Solucioncooperativa.sql
+++ U5/Solucioncooperativa.sql
@@ -70,6 +70,131 @@ delete from socios
 								from entregas)
 	  and numero not in (select distinct socio
 									from compras);    
+                                    
+-- 14
+-- Averigura el socio con saldo más alto
+select numero
+	from socios
+    order by saldo desc
+    limit 1;
+-- Averiguar el id del meterial de precio menor
+select id
+	from materiales
+    order by precio
+    limit 1;
+-- El precio menor
+select min(precio)
+	from materiales;
+-- Insertar compra
+insert into compras(fecha,socio,material,cantidad,precio) 
+	values (curdate(),
+		(select numero
+				from socios
+				order by saldo desc
+				limit 1),
+        (select id
+			from materiales
+			order by precio
+			limit 1),
+        2,
+        2*(select min(precio)
+				from materiales)
+);
+
+-- 14_2
+-- Averigura el socio con saldo más alto
+-- --->Averigura el saldo más alto
+select max(saldo)
+	from socios;
+ -- --->Averigura los socios con saldo anterior   
+select numero 
+	from socios     
+    where saldo = (select max(saldo)
+						from socios);
+-- Averiguar el id del meterial de precio menor
+select id 
+	from materiales      
+    where precio = (select min(precio)
+						from materiales);
+-- Borrar
+delete from compras
+	where socio IN (select numero 
+						from socios     
+						where saldo = (select max(saldo)
+											from socios)) and
+		material IN (select id 
+							from materiales      
+							where precio = (select min(precio)
+												from materiales));  
+-- 15
+-- Socio con más entregas
+set @socio = (select socio
+	from entregas
+    group by socio
+    order by count(*) desc
+    limit 1);
+select @socio;    
+    
+-- Otra forma de obtener el socio con más entregas 
+
+-- ---> Obtener nº de entragas por socios
+select socio, count(*) as num
+	from entregas
+    group by socio;
+-- ---> Obtener el nº de entregas más alto de un socio    
+select max(num)
+		from (select socio, count(*) as num
+			from entregas
+			group by socio) as sc;
+-- ---> Obtener el socio cuyo nº de entregas sea el anterior
+select socio
+	from entregas
+    group by socio
+    having count(*) = (select max(num)
+		from (select socio, count(*) as num
+			from entregas
+			group by socio) as sc)
+	limit 1;
+-- Producto más vendido
+set @prod = (select producto
+		from entregas
+        group by producto
+        order by count(*) desc
+        limit 1);       
+select @prod;
+-- Precio
+set @precio = (select precio 
+	from productos
+    where id = (select producto
+					from entregas
+					group by producto
+					order by count(*) desc
+					limit 1 ));       
+select @precio;                    
+insert into entregas(fecha,kilos,bultos,socio,producto,precio) values 
+	(curdate(),300,3,
+    @socio,
+    @prod,
+    300*@precio);                                                
+-- 17
+-- Socio/s que más han comprado
+select socio
+	from compras
+    group by socio
+    having count(*) = (select max(num)
+						from (select socio, count(*) as num
+								from compras
+                                group by socio) sc
+						);
+update socios as s1
+	set saldo = saldo - (select sum(precio) from compras where socio = s1.numero)
+    where numero in (select socio
+						from compras
+						group by socio
+						having count(*) = (select max(num)
+											from (select socio, count(*) as num
+													from compras
+													group by socio) sc));
 -- 18
 -- Ids de productos con más de 5 entregas
 select producto
@@ -82,6 +207,22 @@ update productos
 						from entregas
 						group by producto
 						having count(*) > 5);
-
+-- 19 
+-- Damos permisos a prueba@% sobre coopertiva
+grant all
+	on *.*
+    to 'prueba'@'%';
     
+
+start transaction;
+select * from compras for update;
+delete from
+	compras
+    where socio IN (select numero 
+						from socios
+						where saldo = (select min(saldo) from socios));
+commit;                  
+-- 20
+delete from productos
+	where id not in (select distinct producto from entregas);
 
