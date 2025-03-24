36c7a5461031a386d7ef7081fbaadd5aa30658cc
rmmateosv <rmmateosv@gmail.com>
2025-03-24 14:26:51 +0100

    0317

--- U6/01ApuntesRutinas.sql
+++ U6/01ApuntesRutinas.sql
@@ -104,10 +104,114 @@ end//
 select calculadora(6,3)//
 
 -- Realiza una rutina que cree un socio en la bd cooperativa
--- Realiza una rutina que devuelva el mensaje: El socio  X
+-- Que devuelva toda la información del socio creado
+use cooperativa//
+drop procedure if exists crearSocio//
+create procedure crearSocio(pNombre varchar(45),
+							pSaldo float)
+begin
+	insert into socios values(default, pNombre, pSaldo);
+    -- Devolver la información del socio
+    select *
+		from socios where numero = last_insert_id();	
+end//
+-- Crear dos socios usando la rutina
+call crearSocio('Rosa',1000)//
+call crearSocio('Paco',1500)//
+-- Realiza una rutina que devuelva el mensaje: 
+-- El socio  X ha gastado Y y ha ingresado Z
+drop function if exists infoSocio//
+create function infoSocio(pNumero int)
+	returns varchar(100) deterministic
+begin
+	-- Declarar varibles
+    declare vGastos float;
+    declare vIngresos float;
+    declare vNumero int;
+    
+    -- Chequear si existe el socio
+    select numero
+    into vNumero
+		from socios
+        where numero = pNumero;
+	if(vNumero is not null) then
+		-- Obtener gastos
+		select ifnull(sum(precio),0)
+			into vGastos
+			from compras
+			where socio = pNumero;
+		-- Obtener ingresos
+		set vIngresos = (select ifnull(sum(precio),0)
+							from entregas
+							where socio = pNumero);
+		-- Devolver el resultado
+		return concat_ws(' ','El socio', pNumero, 'ha gastado',
+							vGastos,'y ha ingresado ',vIngresos);
+    else
+		-- Socio no existe
+        return 'Socio no existe';
+    end if;
+end//
 
+select infoSocio(1)//
 
--- Cambiar de nuevo a ;
+-- ESTRUCTURAS de CONTROL
+-- Cambiar de nuevo a apuntesT6;
+use apuntesT6//
+-- Crear una rutina que muestre una cuenta atrás
+-- que se inicia en un nº que se pasa por parámetro.
+-- Si el nº es <= 0 debe mostrar el texto
+-- 'El nº no puede ser <=0'
+-- Con loop
+drop procedure if exists cuentaAtras1//
+create procedure cuentaAtras1(pNumero int)
+begin
+	-- Cuenta atrás con loop
+    if pNumero <= 0 then
+		select 'El nº no puede ser <=0';
+    else
+		b1:loop
+			select pNumero;
+            set pNumero = pNumero -1;
+            if pNumero < 1 then
+				-- Terminiar bucle
+                leave b1;
+            end if;            
+		end loop;
+    end if;
+end//
+call cuentaAtras1(3)//
+-- Con WHile
+drop procedure if exists cuentaAtras2//
+create procedure cuentaAtras2(pNumero int)
+begin
+	-- Cuenta atrás con loop
+    if pNumero <= 0 then
+		select 'El nº no puede ser <=0';
+    else
+		while pNumero>=1 do
+			select pNumero;
+            set pNumero = pNumero - 1;
+        end while;
+    end if;
+end//
+call cuentaAtras2(3)//
+
+-- Con Repeat
+drop procedure if exists cuentaAtras3//
+create procedure cuentaAtras3(pNumero int)
+begin
+	-- Cuenta atrás con loop
+    if pNumero <= 0 then
+		select 'El nº no puede ser <=0';
+    else
+		repeat
+			select pNumero;
+            set pNumero = pNumero - 1;
+		until pNumero<1 end repeat;
+    end if;
+end//
+call cuentaAtras3(3)//
+
 delimiter ;
-
 
